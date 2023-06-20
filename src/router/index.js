98294746076f94b484b663aee3dec52ace97c47e
Islamzhon Karimov <karimovi@kolesa.kz>
2023-06-20 21:40:49 +0600

    main omega fix package

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
+import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
-            path: '/',
+            path: '/qr/',
             name: 'home',
             component: HomeView,
         },
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-/// <reference types="vitest" />
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/qr/',
     plugins: [vue()],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
-});
+})
